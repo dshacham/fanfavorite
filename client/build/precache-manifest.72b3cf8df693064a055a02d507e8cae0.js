22f952af48e5e5430fed54ce5f132601563b0127
dshacham <dshacham22@gmail.com>
2020-09-02 12:59:28 +0200

    add build

---
+++ client/build/precache-manifest.72b3cf8df693064a055a02d507e8cae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16855110be2cf297328fc60fee2d4e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8dcb5c643b1e112206",
+    "url": "/static/css/main.96129d87.chunk.css"
+  },
+  {
+    "revision": "8b9e80cffe55dcacc29d",
+    "url": "/static/js/2.020c906b.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/static/js/2.020c906b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8dcb5c643b1e112206",
+    "url": "/static/js/main.137221fa.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
